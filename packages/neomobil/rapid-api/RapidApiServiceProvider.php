e7544d492708b06c76b35ac1fe95b09c47a0cc80
Kornel Tako <takokornel@gmail.com>
2020-01-04 18:46:05 +0100

    Add package with service provider

---
+++ packages/neomobil/rapid-api/RapidApiServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Neomobil\RapidApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class RapidApiServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
