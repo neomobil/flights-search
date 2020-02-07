9b8ef2eaaf876f6e3fd1b5ed9b91c7f6b9daa226
Kornel Tako <takokornel@gmail.com>
2020-02-07 11:06:16 +0100

    Move header to Client creation

--- app/Repositories/AirportsRepository.php
+++ app/Repositories/AirportsRepository.php
@@ -15,7 +15,11 @@ class AirportsRepository implements AirportsRepositoryInterface
     {
         $this->client = new Client([
             'base_uri' => config('rapidapi.url'),
-            'timeout' => 2.0
+            'timeout' => 2.0,
+            'headers' => [
+                'x-rapidapi-host' => config('rapidapi.headers.x-rapidapi-host'),
+                'x-rapidapi-key' => config('rapidapi.headers.x-rapidapi-key'),
+            ]
         ]);
     }
 
@@ -25,10 +29,6 @@ class AirportsRepository implements AirportsRepositoryInterface
     public function get(string $string, string $locale = 'en_EN'): StreamInterface
     {
         $response = $this->client->request('GET', 'airports/search', [
-            'headers' => [
-                'x-rapidapi-host' => config('rapidapi.headers.x-rapidapi-host'),
-                'x-rapidapi-key' => config('rapidapi.headers.x-rapidapi-key'),
-            ],
             'query' => [
                 'locale' => $locale,
                 'query' => $string,
